891e1ff42822dea909b0864bb16685740af74a99
alisherkhann <sherkhanali032@gmail.com>
2025-02-10 02:48:42 +0530

    changed backend path

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'http://localhost:5001/api';
-// export const BASE_URL = 'https://vartalaap-2c2d.onrender.com/api';
-export const SOCKET_URL = 'http://localhost:5001';
-// export const SOCKET_URL = 'https://vartalaap-2c2d.onrender.com/';
+// export const BASE_URL = 'http://localhost:5001/api';
+export const BASE_URL = 'https://vartalaap-2c2d.onrender.com/api';
+// export const SOCKET_URL = 'http://localhost:5001';
+export const SOCKET_URL = 'https://vartalaap-2c2d.onrender.com/';
 
 export const DEFAULT_PROFILE_PICTURE = "https://plus.unsplash.com/premium_vector-1723276520843-4a7b1f939a95?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
